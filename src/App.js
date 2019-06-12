8ef231479c187a98ae8b3f108a49017b83348d7c
Milenko <milenko@hackages.io>
2019-06-12 22:10:13 +0200

    update lock

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './tesla-style.scss';
 import logo from './assets/logo.svg';
-import {TeslaBattery} from "./tesla-battery/TeslaBattery";
+import {TeslaBattery} from './tesla-battery/TeslaBattery';
 
-export class App extends Component {
-  render() {
-    return (
-      <div>
-        <header className="header">
-          <img src="logo" />
-        </header>
-        <div className="wrapper">
-          <TeslaBattery />
-        </div>
+export const App = () => {
+  return (
+    <div>
+      <header className="header">
+        <img src="logo" />
+      </header>
+      <div className="wrapper">
+        <TeslaBattery />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
